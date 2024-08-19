239b15d816fe41a7bd64db580f5f6603fd9bb2e3
Carlos Wei <carlos.wei.hk@gmail.com>
2024-08-19 09:42:59 +0800

    [imx8] build all
    Signed-off-by: Carlos Wei <carlos.wei.hk@gmail.com>

--- myd-jx8mpq/docker_build_yocto.sh
+++ myd-jx8mpq/docker_build_yocto.sh
@@ -2,11 +2,16 @@
 
 sudo chown -R build:build sources
 sudo chown -R build:build downloads
-sudo chmod a+rwx build:build downloads
+sudo chmod a+rwx downloads
+
 git config --global --add safe.directory '*'
 
 source setup-environment
 
 EULA=1 DISTRO=fsl-imx-xwayland MACHINE=myd-jx8mp source sources/meta-myir/tools/myir-setup-release.sh -b build-xwayland
+
+sudo chown -R build:build sstate-cache
+sudo chmod a+rwx sstate-cache
+
 bitbake myir-image-full
 
